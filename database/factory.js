fdb7a5e82108a75b60e5f7badefd39676f3485b5
quentin basset <quentin.basset@ochelys.com>
2020-09-12 18:23:10 +0200

    :sparkles: add factory for apartment

--- database/factory.js
+++ database/factory.js
@@ -11,11 +11,13 @@
 |
 */
 
-/** @type {import('@adonisjs/lucid/src/Factory')} */
-// const Factory = use('Factory')
+ const Factory = use('Factory')
 
-// Factory.blueprint('App/Models/User', (faker) => {
-//   return {
-//     username: faker.username()
-//   }
-// })
+Factory.blueprint('App/Models/Apartment', async faker => {
+  return {
+    name: faker.string(),
+    street: faker.street(),
+    zipCode: faker.zip(),
+    city: faker.city()
+  }
+})
