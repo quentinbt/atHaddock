23ea2a72c859fdf3ed9ea13e3d800b6ef2e828b1
quentin basset <quentin.basset@ochelys.com>
2020-09-15 15:50:11 +0200

    :sparkles: add middleware for validators

--- start/routes.js
+++ start/routes.js
@@ -20,9 +20,18 @@ Route.get('/', () => {
 })
 
 Route.resource('apartments', 'ApartmentController').middleware(['auth:jwt'])
+  .validator(new Map([
+    [['apartments.store'], ['StoreApartment']],
+    [['apartments.update'], ['UpdateApartment']]
+  ]))
 Route.resource('apartments.rooms', 'RoomController').middleware(['auth:jwt'])
+  .validator(new Map([
+    [['apartments.rooms.store'], ['StoreRoom']],
+    [['apartments.rooms.update'], ['UpdateRoom']]
+  ]))
 Route.get('whoami', 'UserController.whoami').middleware(['auth:jwt'])
 Route.post('apartments/:apartments_id/rooms/:id/rent', 'RoomController.rent').middleware(['auth:jwt'])
 Route.post('apartments/:apartments_id/rooms/:id/unrent', 'RoomController.unrent').middleware(['auth:jwt'])
 Route.post('login', 'UserController.login')
+  .validator('Login')
 Route.post('signup', 'UserController.signup')
